5e36ec5b45f22aa8fd6111a26c3dac46210b4a77
nilcy <takehiro.shimokawa@gmail.com>
2023-04-07 22:19:11 +0900

    apply MVP.css

---
+++ packages/kamuy-web/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<header>
+	<nav>
+		<a href="/"><img alt="Logo" src="https://via.placeholder.com/200x70?text=Logo" height="70" /></a
+		>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li>
+				<a href="/">=</a>
+				<ul>
+					<li><a href="/">Login</a></li>
+					<li><a href="/">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</header>
+<main>
+	<slot />
+</main>
+<footer>
+	<hr />
+	<p>
+		<small>Contact info</small>
+	</p>
+</footer>
