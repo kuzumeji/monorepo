814bc82ccc4466b917403511e689df3434baac5a
nilcy <takehiro.shimokawa@gmail.com>
2023-04-17 17:31:08 +0900

    一旦リセット

--- packages/kamuy-mvp/src/app.d.ts
+++ packages/kamuy-mvp/src/app.d.ts
@@ -1,25 +1,12 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-import { PrismaClient } from '@prisma/client';
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			user: {
-				id: number;
-				name: string;
-			};
-		}
+		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
-		interface Platform {
-			env: {
-				DATABASE_URL: process.env.DATABASE_URL;
-				NODE_VERSION: process.env.NODE_VERSION;
-			};
-		}
 	}
-	let db: PrismaClient;
 }
 
 export {};
