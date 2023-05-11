2e2a50d082cff2f347ed9ead604adde2c0af3670
nilcy <takehiro.shimokawa@gmail.com>
2023-05-11 11:09:13 +0900

    シンプル化

--- packages/kamuy-api/src/server.ts
+++ packages/kamuy-api/src/server.ts
@@ -2,19 +2,17 @@ import { createYoga } from 'graphql-yoga'
 import { createServer } from 'http'
 import { schema } from './schema'
 
-const yoga = createYoga({
-  graphqlEndpoint: '/',
-  schema,
-  context: (req) => {
-    return {
-      req,
-    }
-  },
-})
-
-const server = createServer(yoga)
-
-server.listen(4000, () => {
+createServer(
+  createYoga({
+    graphqlEndpoint: '/',
+    schema,
+    context: (req) => {
+      return {
+        req,
+      }
+    },
+  }),
+).listen(4000, () => {
   console.log(`\
 🚀 Server ready at: http://localhost:4000
 ⭐️ See sample queries: http://pris.ly/e/ts/graphql#using-the-graphql-api
